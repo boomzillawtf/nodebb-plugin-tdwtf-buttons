76f8decf72921235ac8dd5836c1f7944efcb03b8
NedFodder <shelbyvt98@yahoo.com>
2016-04-01 12:51:39 -0400

    Added files via upload

---
+++ buttons.js
@@ -0,0 +1,23 @@
+$('document').ready(function() {
+	require(['composer', 'composer/controls'], function(composer, controls) {
+		composer.addButton('fa fa-eraser', function(textarea, selectionStart, selectionEnd) {
+			if (selectionStart === selectionEnd) {
+				controls.insertIntoTextarea(textarea, '<del></del><ins></ins>');
+				controls.updateTextareaSelection(textarea, selectionStart + 5, selectionStart + 5);
+			} else {
+				controls.wrapSelectionInTextareaWith(textarea, '<del>', '</del><ins>text</ins>');
+				controls.updateTextareaSelection(textarea, selectionEnd + 16, selectionEnd + 20);
+			}
+		});
+		composer.addButton('fa fa-info-circle', function(textarea, selectionStart, selectionEnd) {
+			if (selectionStart === selectionEnd) {
+				controls.insertIntoTextarea(textarea, '<abbr title="title"></abbr>');
+				controls.updateTextareaSelection(textarea, selectionStart + 13, selectionStart + 18);
+			} else {
+				controls.wrapSelectionInTextareaWith(textarea, '<abbr title="', '">Text</abbr>');
+				controls.updateTextareaSelection(textarea, selectionEnd + 15, selectionEnd + 19);
+			}
+		});
+	});
+});
+
---
+++ library.js
@@ -0,0 +1,14 @@
+// Empty for now, eventually code to parse posts will go here
+
+'use strict';
+
+var plugins = module.parent.require('./plugins'),
+	fs = require('fs'),
+	path = require('path'),
+	util = require('util'),
+	app;
+
+var plugin = {};
+
+module.exports = plugin;
+

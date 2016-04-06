996061e99f85f47a6131f815dbddba3e4d63b724
NedFodder <shelbyvt98@yahoo.com>
2016-04-06 16:24:15 -0400

    Fix spoilers with line breaks.

--- library.js
+++ library.js
@@ -26,9 +26,13 @@ plugin.parse = function(data, callback) {
 
 // replace [spoiler] fake tags with real tags
 function parser(data) {
-	data = data
-		.replace('[spoiler]', '<span class="spoiler blur" onclick="toggleBlur(this);">')
-		.replace('[/spoiler]', '</span>');
+	// replace inline spoilers
+	var spoilers = /\[spoiler\].*?\[\/spoiler\]/g;
+	data = data.replace(spoilers, function(match) {
+		return '<span class="spoiler blur" onclick="toggleBlur(this);">' + match.substring(9, match.length - 10) + '</span>';
+	});
+	// replace multi-line spoilers
+	data = data.replace('[spoiler]', '<div class="spoiler blur" onclick="toggleBlur(this);">').replace('[/spoiler]', '</div>');
 	return data;
 }
 
